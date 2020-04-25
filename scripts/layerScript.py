622bca85492c996df7ada8551b137cd95f09beda
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-04-24 22:22:37 -0400

    Finished layer outputting to jpgs, need to fix input number layers and file path so it's not hard coded

--- scripts/layerScript.py
+++ scripts/layerScript.py
@@ -4,45 +4,56 @@ import maya.mel as mel
 import maya.app.general.createImageFormats as createImageFormats
 
 
-def setUpScene():
-    rs = renderSetup.model.renderSetup.instance()
-    renderSetup.model.renderSetup.initialize()
-    ov = renderSetup.model.override
+class Render:
+    def __init__(self):
+        self.rs = renderSetup.model.renderSetup.instance()
+       
     
-    layer_obj = rs.createRenderLayer('MyFirstLayer')
-    collec_obj = layer_obj.createCollection('MyFirstCollection')
-    over_obj = collec_obj.createOverride('MyFirstOverride', ov.AbsOverride.kTypeId) #absolute   
+    def setUpRenderer(self):
+        cmds.setAttr("defaultRenderGlobals.currentRenderer", "mayaHardware2", type="string")
+        cmds.setAttr("hardwareRenderingGlobals.renderMode", 2)
+        
+        cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
+        cmds.setAttr('defaultRenderGlobals.outFormatControl', 0)
+        
+        
+    def renderAndSave(self, image_save_path):    
+        #cmds.ogsRender(camera='top') #noRenderView=False, currentFrame=True)
+        #cmds.render('top', x=768, y=576 )
+        mel.eval('renderWindowRender redoPreviousRender renderView')
+        mel.eval('updateRenderOverride')
+        editor = 'renderView'
+        formatManager = createImageFormats.ImageFormats()
+        formatManager.pushRenderGlobalsForDesc("JPEG")
+        # cmds.renderWindowEditor(editor, currentCamera='top', e=True, writeImage='/Volumes/Seagate/bob_test/testImage.jpg')
+        cmds.renderWindowEditor(editor, currentCamera='top', e=True, writeImage=image_save_path)
+        formatManager.popRenderGlobals()
+        
     
-    collec_obj.getSelector().staticSelection.set(['grid', 'layer07'])
-    over_obj.finalize('defaultRenderQuality.shadingSamples')
-    
-    rs.switchToLayer(layer_obj)
-    
+    def renderLayers(self, num_max_layers):
+        renderSetup.model.renderSetup.initialize()
+        ov = renderSetup.model.override
+        
+        for i in range(num_max_layers):
+            layer_name = 'layer' + str(i).zfill(2)
+            collection_name = 'collection' + str(i)
+            layer_obj = self.rs.createRenderLayer(layer_name)
+            collec_obj = layer_obj.createCollection(collection_name)
+        
+            collec_obj.getSelector().staticSelection.set(['grid', layer_name])
+        
+            over_obj = collec_obj.createOverride('MyFirstOverride', ov.AbsOverride.kTypeId) #absolute   
+            over_obj.finalize('defaultRenderQuality.shadingSamples')
+        
+            self.rs.switchToLayer(layer_obj)
+            self.renderAndSave('/Volumes/Seagate/bob_test/'+ layer_name + '.jpg')
 
-def setUpRenderer():
-    cmds.setAttr("defaultRenderGlobals.currentRenderer", "mayaHardware2", type="string")
-    cmds.setAttr("hardwareRenderingGlobals.renderMode", 2)
-    
-    cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
-    cmds.setAttr('defaultRenderGlobals.outFormatControl', 0)
-    
-    
-def renderAndSave():    
-    #cmds.ogsRender(camera='top') #noRenderView=False, currentFrame=True)
-    #cmds.render('top', x=768, y=576 )
-    mel.eval('renderWindowRender redoPreviousRender renderView')
-    mel.eval('updateRenderOverride')
-    editor = 'renderView'
-    formatManager = createImageFormats.ImageFormats()
-    formatManager.pushRenderGlobalsForDesc("JPEG")
-    cmds.renderWindowEditor(editor, currentCamera='top', e=True, writeImage='/Volumes/Seagate/bob_test/testImage.jpg')
-    formatManager.popRenderGlobals()
 
 if __name__ == '__main__':
+    r = Render()
     cmds.polyPlane(subdivisionsX=100, subdivisionsY=100, width=100, height=100, name='grid')
-    setUpScene()
-    setUpRenderer()
-    renderAndSave()
+    r.setUpRenderer()
+    r.renderLayers(28)
     
 
 
